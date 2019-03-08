524a6d3331f6ba9aba7fcd0117898bbbc7bb6f57
Markus <markus.andreas@spoe.at>
2019-03-08 14:27:50 +0100

    Explicitly load ActiveSupport extensions.

--- lib/rails_admin/extensions/paper_trail.rb
+++ lib/rails_admin/extensions/paper_trail.rb
@@ -1,3 +1,4 @@
+require "active_support/core_ext/string/strip"
 require 'rails_admin/extensions/paper_trail/auditing_adapter'
 
 RailsAdmin.add_extension(:paper_trail, RailsAdmin::Extensions::PaperTrail, auditing: true)
